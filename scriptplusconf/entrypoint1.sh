26b9acfe534012d74cd5b4367163df023f6648af
gosndka <92586328+gosndka@users.noreply.github.com>
2022-06-12 19:06:07 +0530

    Update entrypoint1.sh

--- scriptplusconf/entrypoint1.sh
+++ scriptplusconf/entrypoint1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Scripted By DevAnaZ"
-echo "Orgin repository : https://github.com/developeranaz/qbittorrent-to-rclone-heroku"
+echo "Orgin repository : https://github.com/gosndka/gscsw"
 mkdir /.config
 mkdir /.config/rclone
 curl -L $RCLONE_CONFIG_IN_RAW_URL >/.config/rclone/rclone.conf
@@ -8,7 +8,7 @@ echo $PORT >/PORT
 #curl -L $QBITTORRENT_CONFIG_IN_RAW_URL
 cat /qBittorrent/config/qBittorrent.conf >/qBittorrent/qBittorrent.conf
 echo -n "$WEBPASSWORD" |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
-cat /qBittorrent/qBittorrent.conf |sed "s|devanaz:|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSER|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
+cat /qBittorrent/qBittorrent.conf |sed "s|devanaz:|$(rclone listremotes |head -1)|g" |sed "s|gosndka|$WEBUSER|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
 
 while :
 do
